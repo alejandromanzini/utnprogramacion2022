8c26347f29a14b9b1d1e736d64ccdc1f6259e578
alejandromanzini <99147901+alejandromanzini@users.noreply.github.com>
2022-05-19 19:07:17 -0300

    Trabajo en clase

---
+++ M4U4/routes/admin/novedades.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var usuariosModel = require('../../models/usuariosModels');
+
+/* GET home page. */
+router.get('/', function(req, res,) {
+  res.render('admin/novedades', { 
+      layout: 'admin/layout',
+    usuario: req.session.nombre });
+});
+
+module.exports = router;
