c457967015dba72557746917733c21c555e6ac5b
alejandromanzini <99147901+alejandromanzini@users.noreply.github.com>
2022-05-26 15:43:56 -0300

    U5M1

--- M4U4/routes/admin/novedades.js
+++ M4U4/routes/admin/novedades.js
@@ -1,12 +1,26 @@
 var express = require('express');
 var router = express.Router();
-var usuariosModel = require('../../models/usuariosModels');
+var novedadesModel = require ('../../models/novedadesModel');
 
 /* GET home page. */
-router.get('/', function(req, res,) {
+router.get('/', async function(req, res,) {
+  var novedades = await novedadesModel.getNovedades();
+  
+  
+  
+  
   res.render('admin/novedades', { 
       layout: 'admin/layout',
-    usuario: req.session.nombre });
+    usuario: req.session.nombre,
+  novedades
+ });
+});
+
+router.get('eliminar/:id', async (req,res,next) => {
+  var id = req.params.id;
+  
+  await novedadesModel.deleteNovedadesById(id);
+  res.redirect('/admin/novedades')
 });
 
 module.exports = router;
