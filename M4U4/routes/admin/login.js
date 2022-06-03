d3275c26497962817c48ca395a98cb23b9a6dfb9
alejandromanzini <99147901+alejandromanzini@users.noreply.github.com>
2022-06-03 00:54:24 -0300

    U5M2

--- M4U4/routes/admin/login.js
+++ M4U4/routes/admin/login.js
@@ -26,6 +26,7 @@ router.post ('/', async (req, res, next) => {
         if (data != undefined) {
             req.session.id_usuario = data.id;
             req.session.nombre = data.usuario;
+            
             res.redirect('/admin/novedades');
         } else {
             res.render ('admin/login', {
